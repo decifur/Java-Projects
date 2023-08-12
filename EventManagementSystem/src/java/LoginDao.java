fe51a8d0fd2b4327b2a7e4f6b12d72ddc83ae654
Jitesh Srivastav <90095692+decifur@users.noreply.github.com>
2023-08-12 16:58:40 +0530

    fixed error in Validation

--- EventManagementSystem/src/java/LoginDao.java
+++ EventManagementSystem/src/java/LoginDao.java
@@ -5,10 +5,8 @@ public static boolean validate(String USER_NAME,String PASS_WORD){
 boolean status=false;  
 try{  
     Class.forName("oracle.jdbc.driver.OracleDriver");  
-    Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","abhi1234");  
-    PreparedStatement ps=con.prepareStatement("select * from indexdeatils where USER_NAME=? and PASS_WORD=?");  
-    ps.setString(1,USER_NAME);  
-    ps.setString(2,PASS_WORD);  
+    Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","abhi1234");
+    PreparedStatement ps=con.prepareStatement("select * from indexdetails where USER_NAME=\'"+USER_NAME+"\' and PASS_WORD=\'"+PASS_WORD+"\'");   
     ResultSet rs=ps.executeQuery();  
     status=rs.next();
     con.close();
