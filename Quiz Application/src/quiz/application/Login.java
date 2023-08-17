7e2b81f885b17b3a6a44506a06ad42076817cb21
Jitesh Srivastav <90095692+decifur@users.noreply.github.com>
2023-08-17 19:16:14 +0530

    created login java file

--- Quiz Application/src/quiz/application/Login.java
+++ Quiz Application/src/quiz/application/Login.java
@@ -2,34 +2,69 @@ package quiz.application;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class Login extends JFrame {
+public class Login extends JFrame implements ActionListener{
+ 
+    JButton rules, back;
+    JTextField tfname;
     
-    Login() {      
-        // Get the dimensions of the screen
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        
-        // Setting size of the Frame
-        setSize(screenSize.width - screenSize.width/4, screenSize.height - screenSize.height/3);
-        // Setting color of the Frame
+    Login() {
         getContentPane().setBackground(Color.WHITE);
-        
-        // Calculate the center coordinates
-        int x = (screenSize.width - getWidth()) / 2;
-        int y = (screenSize.height - getHeight()) / 2;      
-        setLocation(x, y);  // Set the location at the center of the screen
-        
-        ImageIcon il = new ImageIcon(ClassLoader.getSystemResource("icons/login.jpeg"));
-        JLabel image = new JLabel(il);
         setLayout(null);
-        image.setBounds(0,0,getWidth()/2,getHeight());
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/login.jpeg"));
+        JLabel image = new JLabel(i1);
+        image.setBounds(0, 0, 600, 500);
         add(image);
         
+        JLabel heading = new JLabel("Simple Minds");
+        heading.setBounds(750, 60, 300, 45);
+        heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 40));
+        heading.setForeground(new Color(30, 144, 254));
+        add(heading);
+        
+        JLabel name = new JLabel("Enter your name");
+        name.setBounds(810, 150, 300, 20);
+        name.setFont(new Font("Mongolian Baiti", Font.BOLD, 18));
+        name.setForeground(new Color(30, 144, 254));
+        add(name);
+        
+        tfname = new JTextField();
+        tfname.setBounds(735, 200, 300, 25);
+        tfname.setFont(new Font("Times New Roman", Font.BOLD, 20));
+        add(tfname);
+        
+        rules = new JButton("Rules");
+        rules.setBounds(735, 270, 120, 25);
+        rules.setBackground(new Color(30, 144, 254));
+        rules.setForeground(Color.WHITE);
+        rules.addActionListener(this);
+        add(rules);
+        
+        back = new JButton("Back");
+        back.setBounds(915, 270, 120, 25);
+        back.setBackground(new Color(30, 144, 254));
+        back.setForeground(Color.WHITE);
+        back.addActionListener(this);
+        add(back);
+        
+        setSize(1200, 500);
+        setLocation(200, 150);
         setVisible(true);
     }
     
-    public static void main(String[] args){
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == rules) {
+            String name = tfname.getText();
+            setVisible(false);
+            new Rules(name);
+        } else if (ae.getSource() == back) {
+            setVisible(false);
+        }
+    }
+    
+    public static void main(String[] args) {
         new Login();
     }
 }
-
